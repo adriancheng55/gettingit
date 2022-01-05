07a5627ff31231a13145d8c080e99f22814d644e
Adrian Cheng <55768495+adriancheng55@users.noreply.github.com>
2022-01-05 15:43:12 -0500

    Made some more design changes

--- node.js
+++ node.js
@@ -339,6 +339,12 @@ function chartStuff()
 function percentStuff()
 {
 
-	document.getElementById("percentage").innerHTML = "Well... " + localStorage.getItem("keyName") + "! ";
+	document.getElementById("percentage").innerHTML = "Well... " + localStorage.getItem("keyName");
 	document.getElementById("moreText").innerHTML =  "Your chance of getting it is " + localStorage.getItem("keyPercent") + "%";
+	ducment.getElementById("")
+}
+
+function goBack()
+{
+	window.location.href = "index.html";
 }
